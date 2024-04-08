c43ee2d3a34829b49564572370b024c7243cedcf
devika-bej <devika.bej@research.iiit.ac.in>
2024-04-09 02:50:18 +0530

    tried making a line chart, with somewhat grids

--- chess/src/App.js
+++ chess/src/App.js
@@ -1,8 +1,90 @@
-import "./index.css"
+import LineChart from "./LineChart";
+import "./index.css";
 function App() {
-  return <div className="">
-    <h1 > chess</h1>
-  </div>;
+  return (
+    <div className="grid grid-rows-2 grid-cols-8 justify-items-center h-[90vh] w-[90vw]">
+      <LineChart
+        pieceName="Pawn"
+        movesWhite={["e2", "e4", "e4", "e4", "f5"]}
+        movesBlack={["e7", "e5", "e5", "d4", "c3"]}
+      ></LineChart>
+      <LineChart
+        pieceName="Pawn"
+        movesWhite={["e2", "e4", "e4", "e4", "f5"]}
+        movesBlack={["e7", "e5", "e5", "d4", "c3"]}
+      ></LineChart>
+      <LineChart
+        pieceName="Pawn"
+        movesWhite={["e2", "e4", "e4", "e4", "f5"]}
+        movesBlack={["e7", "e5", "e5", "d4", "c3"]}
+      ></LineChart>
+      <LineChart
+        pieceName="Pawn"
+        movesWhite={["e2", "e4", "e4", "e4", "f5"]}
+        movesBlack={["e7", "e5", "e5", "d4", "c3"]}
+      ></LineChart>
+      <LineChart
+        pieceName="Pawn"
+        movesWhite={["e2", "e4", "e4", "e4", "f5"]}
+        movesBlack={["e7", "e5", "e5", "d4", "c3"]}
+      ></LineChart>
+      <LineChart
+        pieceName="Pawn"
+        movesWhite={["e2", "e4", "e4", "e4", "f5"]}
+        movesBlack={["e7", "e5", "e5", "d4", "c3"]}
+      ></LineChart>
+      <LineChart
+        pieceName="Pawn"
+        movesWhite={["e2", "e4", "e4", "e4", "f5"]}
+        movesBlack={["e7", "e5", "e5", "d4", "c3"]}
+      ></LineChart>
+      <LineChart
+        pieceName="Pawn"
+        movesWhite={["e2", "e4", "e4", "e4", "f5"]}
+        movesBlack={["e7", "e5", "e5", "d4", "c3"]}
+      ></LineChart>
+      <LineChart
+        pieceName="Pawn"
+        movesWhite={["e2", "e4", "e4", "e4", "f5"]}
+        movesBlack={["e7", "e5", "e5", "d4", "c3"]}
+      ></LineChart>
+      <LineChart
+        pieceName="Pawn"
+        movesWhite={["e2", "e4", "e4", "e4", "f5"]}
+        movesBlack={["e7", "e5", "e5", "d4", "c3"]}
+      ></LineChart>
+      <LineChart
+        pieceName="Pawn"
+        movesWhite={["e2", "e4", "e4", "e4", "f5"]}
+        movesBlack={["e7", "e5", "e5", "d4", "c3"]}
+      ></LineChart>
+      <LineChart
+        pieceName="Pawn"
+        movesWhite={["e2", "e4", "e4", "e4", "f5"]}
+        movesBlack={["e7", "e5", "e5", "d4", "c3"]}
+      ></LineChart>
+      <LineChart
+        pieceName="Pawn"
+        movesWhite={["e2", "e4", "e4", "e4", "f5"]}
+        movesBlack={["e7", "e5", "e5", "d4", "c3"]}
+      ></LineChart>
+      <LineChart
+        pieceName="Pawn"
+        movesWhite={["e2", "e4", "e4", "e4", "f5"]}
+        movesBlack={["e7", "e5", "e5", "d4", "c3"]}
+      ></LineChart>
+      <LineChart
+        pieceName="Pawn"
+        movesWhite={["e2", "e4", "e4", "e4", "f5"]}
+        movesBlack={["e7", "e5", "e5", "d4", "c3"]}
+      ></LineChart>
+      <LineChart
+        pieceName="Pawn"
+        movesWhite={["e2", "e4", "e4", "e4", "f5"]}
+        movesBlack={["e7", "e5", "e5", "d4", "c3"]}
+      ></LineChart>
+    </div>
+  );
 }
 
 export default App;
---
+++ chess/src/LineChart.js
@@ -0,0 +1,68 @@
+import { useEffect, useRef } from "react";
+import * as d3 from "d3";
+
+function LineChart({ pieceName, movesWhite, movesBlack }) {
+  const chartRef = useRef();
+
+  var letters = ["a", "b", "c", "d", "e", "f", "g", "h"];
+  var numbers = ["1", "2", "3", "4", "5", "6", "7", "8"];
+  var positions = [];
+  for (var i = 0; i < 8; ++i)
+    for (var j = 0; j < 8; ++j) positions.push(letters[i] + numbers[j]);
+
+  useEffect(() => {
+    const margin = { top: 10, right: 0, bottom: 10, left: 5 };
+    const width = (window.innerWidth * 0.8) / 8 - margin.left - margin.right;
+    const height = window.innerHeight * 0.4 - margin.top - margin.bottom;
+    const chart = d3.select(chartRef.current);
+    chart.selectAll("*").remove();
+    chart
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom);
+
+    const xScale = d3
+      .scaleLinear()
+      .domain([0, movesWhite.length])
+      .range([0, width]);
+    const yScale = d3.scaleLinear().domain(positions).range([height, 0]);
+
+    const g = chart
+      .append("g")
+      .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    const lineWhite = d3
+      .line()
+      .x((d, i) => xScale(i))
+      .y((d) => yScale(d));
+    const lineBlack = d3
+      .line()
+      .x((d, i) => xScale(i))
+      .y((d) => yScale(d));
+
+    g.append("path")
+      .datum(movesWhite)
+      .attr("stroke", "white")
+      .attr("stroke-width", 3)
+      .attr("d", lineWhite);
+    g.append("path")
+      .datum(movesBlack)
+      .attr("stroke", "black")
+      .attr("stroke-width", 3)
+      .attr("d", lineBlack);
+    g.append("g")
+      .attr("transform", `translate(${margin.left},${height - margin.bottom})`)
+      .call(d3.axisBottom(xScale));
+    g.append("g")
+      .attr("transform", `translate(${margin.left},0)`)
+      .call(d3.axisLeft(yScale));
+  }, [movesWhite, movesBlack]);
+
+  return (
+    <div className="bg-pink-200 w-full border-black border-2 h-full">
+      <h1>{pieceName}</h1>
+      <svg ref={chartRef} className=""></svg>
+    </div>
+  );
+}
+
+export default LineChart;
