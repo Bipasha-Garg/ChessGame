bb6ec37d894bf34522432b539564a247053e650d
devika-bej <devika.bej@research.iiit.ac.in>
2024-04-10 16:29:41 +0530

    commenting on line chart

--- vite-chess/src/LineChart.jsx
+++ vite-chess/src/LineChart.jsx
@@ -8,18 +8,23 @@ function LineChart({
   positions,
   noOfMoves,
 }) {
+  // reference for the chart
   const chartRef = useRef();
 
   useEffect(() => {
+    // declare dimensions for the chart
     const margin = { top: 10, right: 10, bottom: 10, left: 10 };
     const width = (window.innerWidth * 0.9) / 8 - margin.left - margin.right;
     const height = window.innerHeight * 0.4 - margin.top - margin.bottom;
+
+    // creating the chart
     const chart = d3.select(chartRef.current);
     chart.selectAll("*").remove();
     chart
       .attr("width", width + margin.left + margin.right)
       .attr("height", height + margin.top + margin.bottom);
 
+    // setting up the scaling for x and y axes
     const xScale = d3
       .scaleLinear()
       .domain([0, noOfMoves])
@@ -29,10 +34,12 @@ function LineChart({
       .domain([1, 64])
       .range([height - margin.bottom, 0]);
 
+    // creating group of svg for chart
     const g = chart
       .append("g")
       .attr("transform", `translate(${margin.left},${margin.top})`);
 
+    // line generator for moves of white piece and black piece
     const lineWhite = d3
       .line()
       .x((d, i) => xScale(i))
@@ -42,6 +49,7 @@ function LineChart({
       .x((d, i) => xScale(i))
       .y((d) => yScale(positions.indexOf(d) + 1));
 
+    // render the line and dots for white piece
     g.append("path")
       .datum(movesWhite)
       .attr("stroke", "white")
@@ -61,6 +69,8 @@ function LineChart({
       })
       .attr("r", 2)
       .attr("fill", "white");
+
+    // render the line and dots for black piece
     g.append("path")
       .datum(movesBlack)
       .attr("stroke", "black")
@@ -81,6 +91,7 @@ function LineChart({
       .attr("r", 2)
       .attr("fill", "black");
 
+    // render the axes
     var xAxis = d3.axisBottom(xScale).ticks(noOfMoves + 1);
     var yAxis = d3
       .axisLeft(yScale)
@@ -91,6 +102,7 @@ function LineChart({
       .style("font-size", "8px")
       .attr("transform", `translate(${margin.left},${height - margin.bottom})`)
       .call(xAxis)
+      // tilt the labels
       .selectAll("text")
       .style("text-anchor", "end")
       .attr("dx", "-1em")
